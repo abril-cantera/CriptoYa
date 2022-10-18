a78a8f156491c4135a70577bce01b2bd8a2393da
Abril-cantera <abril.gonzalez.cantera@gmail.com>
2022-10-18 14:25:36 -0300

    añadiendo res a html

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-
-
-const test = "https://criptoya.com/api/kriptonmarket/doc/ars/100";
-
-
-
 async function consultarExchange() {
 
 
@@ -14,6 +8,14 @@ async function consultarExchange() {
   var coin = document.getElementById('coin').value;
   var fiat = document.getElementById('fiat').value;
   var volumen = document.getElementById('volumen').value;
+  var ask = document.getElementById('ask')
+  var totalAsk = document.getElementById('totalAsk')
+  var bid = document.getElementById('bid')
+  var time = document.getElementById('time')
+  var exchangeValueInput = document.getElementById('exchangeValueInput')
+  var coinValueInput = document.getElementById('coinValueInput')
+  var fiatValueInput = document.getElementById('fiatValueInput')
+  var volumenValueInput = document.getElementById('volumenValueInput')
 
   console.log(exchange);
   console.log(coin);
@@ -22,13 +24,34 @@ async function consultarExchange() {
 
   const apiExchange = `${api}/${exchange}/${coin}/${fiat}/${volumen}`;
 
-
-  fetch(apiExchange)
-    .then(res => res.json())
-    .then(res => console.log(res))
   
+ fetch(apiExchange)
+   .then(res => res.json())
+   .then(res => {
+     exchangeValueInput.innerHTML = `${exchange}`
+     coinValueInput.innerHTML = `${coin}`
+     fiatValueInput.innerHTML = `${fiat}`
+     volumenValueInput.innerHTML = `${volumen}`
 
+
+     ask.innerHTML = `${res.ask}`
+     totalAsk.innerHTML = `${res.totalAsk}`
+     bid.innerHTML = `${res.bid}`
+     time.innerHTML = `${res.time}`
+     totalBid.innerHTML = `${res.totalBid}`
+   })
+    .catch(err => console.log(err)) 
+  
+  // const res =  await fetch(apiExchange)
+  
+  // const data = await res.json()
+  
+  // console.log(data);
+
+  // test.innerHTML = `${data.ask}`
 
   console.log('Exchange');
+  
   console.log(apiExchange);
 }
+  
