635f971cdf943628bf4e89d626f9ac0abfd57318
Abril-cantera <abril.gonzalez.cantera@gmail.com>
2022-10-21 15:28:14 -0300

    Modificaciones terminadas, agregue limpieza del formulario despues de consultar

--- main.js
+++ main.js
@@ -16,42 +16,55 @@ async function consultarExchange() {
   var coinValueInput = document.getElementById('coinValueInput')
   var fiatValueInput = document.getElementById('fiatValueInput')
   var volumenValueInput = document.getElementById('volumenValueInput')
+  var form = document.getElementById('form')
 
-  console.log(exchange);
+  /* console.log(exchange);
   console.log(coin);
   console.log(fiat);
-  console.log(volumen);
+  console.log(volumen); */
+
 
   const apiExchange = `${api}/${exchange}/${coin}/${fiat}/${volumen}`;
 
-  
- fetch(apiExchange)
-   .then(res => res.json())
-   .then(res => {
-     exchangeValueInput.innerHTML = `${exchange}`
-     coinValueInput.innerHTML = `${coin}`
-     fiatValueInput.innerHTML = `${fiat}`
-     volumenValueInput.innerHTML = `${volumen}`
+
+  fetch(apiExchange)
+    .then(res => res.json())
+    .then(res => {
+      console.log(res);
+
+      exchangeValueInput.innerHTML = `${exchange}`
+      coinValueInput.innerHTML = `${coin}`
+      fiatValueInput.innerHTML = `${fiat}`
+      volumenValueInput.innerHTML = `${volumen}`
 
 
-     ask.innerHTML = `${res.ask}`
-     totalAsk.innerHTML = `${res.totalAsk}`
-     bid.innerHTML = `${res.bid}`
-     time.innerHTML = `${res.time}`
-     totalBid.innerHTML = `${res.totalBid}`
-   })
-    .catch(err => console.log(err)) 
-  
+
+      ask.innerHTML = `${new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(res.ask)
+        }`;
+
+      totalAsk.innerHTML = `${new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(res.totalAsk)} `
+
+      bid.innerHTML = `${new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(res.bid)} `
+
+      totalBid.innerHTML = `${new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' }).format(res.totalBid)} `
+
+      time.innerHTML = `${new Date((res.time) * 1000).toLocaleString('en-US')} `
+
+    })
+    .catch(err => console.log(err))
+
   // const res =  await fetch(apiExchange)
-  
+
   // const data = await res.json()
-  
+
   // console.log(data);
 
-  // test.innerHTML = `${data.ask}`
+  // test.innerHTML = `${ data.ask } `
+
+  form.reset()
 
   console.log('Exchange');
-  
+
   console.log(apiExchange);
 }
-  
+
